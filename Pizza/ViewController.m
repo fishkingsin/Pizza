60675359316aec75c35574944f8b06332eec6aa3
James Kong <jkong@musiclifeltd.com>
2015-04-23 16:02:57 +0800

    update UI

--- Pizza/ViewController.m
+++ Pizza/ViewController.m
@@ -10,6 +10,7 @@
 #import "LGBLuetooth.h"
 #import "Pizza.h"
 #import "MHRotaryKnob.h"
+#import "M13Checkbox.h"
 static NSString * const PizzaServiceUUID = @"13333333-3333-3333-3333-333333333337";
 static NSString * const PizzaCrustCharacteristicUUID = @"13333333-3333-3333-3333-333333330001";
 static NSString * const PizzaToppingsCharacteristicUUID = @"13333333-3333-3333-3333-333333330002";
@@ -24,12 +25,25 @@ static NSString * const PizzaBakeCharacteristicUUID = @"13333333-3333-3333-3333-
 @property(strong,nonatomic) LGCharacteristic *PizzaToppingsCharacteristic;
 @property(strong,nonatomic) LGCharacteristic *PizzaBakeCharacteristic;
 @property(strong,nonatomic) Pizza *pizza;
+@property (strong, nonatomic) IBOutletCollection(M13Checkbox) NSArray *checkboxes;
+@property (strong, nonatomic) NSMutableDictionary *checkboxesDic;
+@property (strong, nonatomic)  NSArray * toppings;
 @end
 
 @implementation ViewController
-
+@synthesize toppings;
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    toppings = @[@"PEPPERONI",
+                 @"MUSHROOMS",
+                 @"EXTRA_CHEESE",
+                 @"BLACK_OLIVES",
+                 @"CANADIAN_BACON",
+                 @"PINEAPPLE",
+                 @"BELL_PEPPERS",
+                 @"SAUSAGE"];
     
     self.rotaryKnob.interactionStyle = MHRotaryKnobInteractionStyleRotating;
     self.rotaryKnob.scalingFactor = 1.5f;
@@ -46,11 +60,27 @@ static NSString * const PizzaBakeCharacteristicUUID = @"13333333-3333-3333-3333-
     self.rotaryKnob.knobImageCenter = CGPointMake(80.0f, 76.0f);
     [self.rotaryKnob addTarget:self action:@selector(rotaryKnobDidChange) forControlEvents:UIControlEventValueChanged];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    int i = 0;
+    self.checkboxesDic = [NSMutableDictionary dictionary];
+    for(M13Checkbox *cb in self.checkboxes)
+    {
+        M13Checkbox* checkbox  = (M13Checkbox*)cb;
+        [checkbox.titleLabel setAdjustsFontSizeToFitWidth:YES];
+        [checkbox.titleLabel setText:[toppings objectAtIndex:i]];
+        [self.checkboxesDic setObject:checkbox forKey:[toppings objectAtIndex:i]];
+        i++;
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(BOOL)isCheckByKey:(NSString*)key
+{
+    return ((M13Checkbox*)[self.checkboxesDic objectForKey:key]).checkState;
 }
 -(void) startBaking
 {
@@ -64,14 +94,14 @@ static NSString * const PizzaBakeCharacteristicUUID = @"13333333-3333-3333-3333-
         {
             unsigned char * raw_data = malloc(2);
             u_int16_t value =
-            PEPPERONI|
-            MUSHROOMS|
-            EXTRA_CHEESE|
-            BLACK_OLIVES|
-            CANADIAN_BACON|
-            PINEAPPLE|
-            BELL_PEPPERS|
-            SAUSAGE;
+            ([self isCheckByKey:[toppings objectAtIndex:0]])?PEPPERONI:NONE|
+            ([self isCheckByKey:[toppings objectAtIndex:1]])?MUSHROOMS:NONE|
+            ([self isCheckByKey:[toppings objectAtIndex:2]])?EXTRA_CHEESE:NONE|
+            ([self isCheckByKey:[toppings objectAtIndex:3]])?BLACK_OLIVES:NONE|
+            ([self isCheckByKey:[toppings objectAtIndex:4]])?CANADIAN_BACON:NONE|
+            ([self isCheckByKey:[toppings objectAtIndex:5]])?PINEAPPLE:NONE|
+            ([self isCheckByKey:[toppings objectAtIndex:6]])?BELL_PEPPERS:NONE|
+            ([self isCheckByKey:[toppings objectAtIndex:7]])?SAUSAGE:NONE;
             raw_data[1]=value & 0xff;
             raw_data[0]=(value >> 8);
             
@@ -87,7 +117,7 @@ static NSString * const PizzaBakeCharacteristicUUID = @"13333333-3333-3333-3333-
                     [weakSelf.PizzaBakeCharacteristic setNotifyValue:YES completion:^(NSError *error) {
                         if(error)
                         {
-                             [self logToTextView:[NSString stringWithFormat:@"%s %@",__PRETTY_FUNCTION__,error]];
+                            [self logToTextView:[NSString stringWithFormat:@"%s %@",__PRETTY_FUNCTION__,error]];
                         }
                         else
                         {
@@ -111,11 +141,11 @@ static NSString * const PizzaBakeCharacteristicUUID = @"13333333-3333-3333-3333-
                             [data getBytes:resultByte length:1];
                             unsigned char result = resultByte[0];
                             [self logToTextView:[NSString stringWithFormat:@"The result is %s",(result == HALF_BAKED) ? "half baked." :
-                                  (result == BAKED) ? "baked." :
-                                  (result == CRISPY) ? "crispy." :
-                                  (result == BURNT) ? "burnt." :
-                                  (result == ON_FIRE) ? "on fire!" :
-                                  "unknown?"]];
+                                                 (result == BAKED) ? "baked." :
+                                                 (result == CRISPY) ? "crispy." :
+                                                 (result == BURNT) ? "burnt." :
+                                                 (result == ON_FIRE) ? "on fire!" :
+                                                 "unknown?"]];
                             [weakSelf.PizzaToppingsCharacteristic readValueWithBlock:^(NSData *data, NSError *error) {
                                 unsigned char* toppingResultByte = malloc(2);
                                 [data getBytes:toppingResultByte length:2];
@@ -127,7 +157,7 @@ static NSString * const PizzaBakeCharacteristicUUID = @"13333333-3333-3333-3333-
                         }
                     }];
                     
-                        
+                    
                     
                 }
             }];
